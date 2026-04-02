70cc82079be9ef57665c58c7b224561bd12ecaec
Michel Tse <michel.tsetseg@gmail.com>
2026-04-02 16:20:32 +0200

    Final Push, full CRUD

--- Pin.Products.Core/Services/Models/CreateOrUpdateProductModel.cs
+++ Pin.Products.Core/Services/Models/CreateOrUpdateProductModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Text.Json.Serialization;
@@ -12,12 +13,16 @@ namespace Pin.Products.Core.Services.Models
         [JsonPropertyName("id")]
         public int Id { get; set; }
         [JsonPropertyName("title")]
+        [Required(ErrorMessage = "Please provide a Title!")]
         public string Title { get; set; }
 
         [JsonPropertyName("price")]
+        [Required(ErrorMessage = "Please provide a price between 1 and 1000!")]
+        [Range(1,1000)]
         public int Price { get; set; }
 
         [JsonPropertyName("description")]
+        [Required(ErrorMessage = "Please provide a Description!")]
         public string Description { get; set; }
 
         [JsonPropertyName("categoryId")]
